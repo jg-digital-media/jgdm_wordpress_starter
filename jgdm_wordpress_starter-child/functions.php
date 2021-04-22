3840aac963f30326caea1e80e47cffe899575dcc
Jonathan Grieve <mail@jonniegrieve.co.uk>
2021-04-22 16:37:25 +0100

    version 2.4.3 add child theme files

---
+++ jgdm_wordpress_starter-child/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+*
+* Load Parent Theme Styles
+*/
+
+
+function myprefix_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'myprefix_theme_enqueue_styles' ); 
+
+/**
+ * 
+ * Remove Dashboard Widgets
+ */
+
+ // Remove dashboard widgets
+function remove_dashboard_meta() {
+	if ( ! current_user_can( 'manage_options' ) ) {
+		remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+		remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');
+	}
+}
+add_action( 'admin_init', 'remove_dashboard_meta' ); 
